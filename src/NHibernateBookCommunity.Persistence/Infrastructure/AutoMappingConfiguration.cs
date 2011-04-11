8419c9b20f13c36a0e779c22c7845f02b4972305
Erik Öjebo <erik.ojebo@gmail.com>
2011-04-11 21:13:00 +0200

    removed conventions and automapping configuration and session factory creation code

--- src/NHibernateBookCommunity.Persistence/Infrastructure/AutoMappingConfiguration.cs
+++ src/NHibernateBookCommunity.Persistence/Infrastructure/AutoMappingConfiguration.cs
@@ -9,14 +9,5 @@ namespace NHibernateBookCommunity.Persistence.Infrastructure
 {
     public class AutoMappingConfiguration : DefaultAutomappingConfiguration
     {
-        public override bool ShouldMap(Type type)
-        {
-            return type.IsSubclassOf(typeof(Entity));
-        }
-
-        public override Access GetAccessStrategyForReadOnlyProperty(Member member)
-        {
-            return Access.CamelCaseField(CamelCasePrefix.Underscore);
-        }
     }
 }
--- src/NHibernateBookCommunity.Persistence/Infrastructure/SessionFactorySingleton.cs
+++ src/NHibernateBookCommunity.Persistence/Infrastructure/SessionFactorySingleton.cs
@@ -4,8 +4,6 @@ using FluentNHibernate.Cfg.Db;
 using NHibernate;
 using NHibernate.Tool.hbm2ddl;
 using NHibernateBookCommunity.Domain.Entities;
-using NHibernateBookCommunity.Persistence.Mapping.Conventions;
-using NHibernateBookCommunity.Persistence.Mapping.Overrides;
 
 namespace NHibernateBookCommunity.Persistence.Infrastructure
 {
@@ -19,30 +17,7 @@ namespace NHibernateBookCommunity.Persistence.Infrastructure
             {
                 if (_factory == null)
                 {
-                    _factory = Fluently.Configure()
-                        .Database(
-                            MsSqlConfiguration.MsSql2005.ConnectionString(
-                                x => x.Server(@".\SQLEXPRESS")
-                                    .Database("nhibernate_book_community")
-                                         .TrustedConnection())
-                                .ShowSql()
-                                //.FormatSql()
-                                )
-                        .Mappings(m => m.AutoMappings.Add(
-                            AutoMap.Assemblies(
-                                new AutoMappingConfiguration(),
-                                typeof(User).Assembly)
-                                           .UseOverridesFromAssemblyOf<ReviewMappingOverride>()
-                                           .Conventions.AddFromAssemblyOf<ForeignKeyColumnNameConvention>()
-                                           )
-                                           .ExportTo(@"c:\temp\mappings"))
-                        .ExposeConfiguration(cfg =>
-                            {
-                                var export = new SchemaExport(cfg);
-                                export.Drop(true, true);
-                                export.Create(true, true);
-                            })
-                        .BuildSessionFactory();
+                    _factory = null; // Implementation here
                 }
 
                 return _factory;
