3f5f7f3a71ac8a3ac8d18357c26188cea98aff16
Erik Öjebo <erik.ojebo@gmail.com>
2011-04-11 20:59:33 +0200

    Added tests for basic repo methods

--- src/NHibernateBookCommunity.Persistence/Repositories/UserRepositorySpecs.cs
+++ src/NHibernateBookCommunity.Persistence/Repositories/UserRepositorySpecs.cs
@@ -31,21 +31,21 @@ namespace NHibernateBookCommunity.Persistence.Repositories
                 {
                     Username = "User 2",
                     Password = "Password 2",
-                    LastLoginDate = new DateTime(2022, 2, 2, 3, 4, 5),
+                    LastLoginDate = new DateTime(2011, 2, 2, 3, 4, 5),
                 };
 
             _user3 = new User
                 {
                     Username = "User 3",
                     Password = "Password 3",
-                    LastLoginDate = new DateTime(3033, 3, 3, 3, 4, 5),
+                    LastLoginDate = new DateTime(2011, 3, 3, 3, 4, 5),
                 };
 
             _user4 = new User
                 {
-                    Username = "User 3",
-                    Password = "Password 3",
-                    LastLoginDate = new DateTime(3033, 3, 3, 3, 4, 5),
+                    Username = "User 4",
+                    Password = "Password 4",
+                    LastLoginDate = new DateTime(2011, 4, 4, 4, 4, 5),
                 };
 
             _user1.AddReview(new Review
@@ -91,6 +91,100 @@ namespace NHibernateBookCommunity.Persistence.Repositories
             SaveAllUsers();
 
             _repository = new UserRepository();
+        }
+
+        [Test]
+        public void Get_returns_the_user_with_the_given_id()
+        {
+            var actual = _repository.Get(_user1.Id);
+
+            Assert.AreEqual(_user1.Id, actual.Id);
+            Assert.AreEqual("User 1", actual.Username);
+        }
+
+        [Test]
+        public void Get_returns_null_for_non_existing_id()
+        {
+            var actual = _repository.Get(-1);
+            Assert.IsNull(actual);
+        }
+
+        [Test]
+        public void GetAll_returns_all_saved_users()
+        {
+            var actual = _repository.GetAll()
+                .OrderBy(x => x.Username);
+
+            Assert.AreEqual(4, actual.Count());
+            Assert.AreEqual("User 1", actual.ElementAt(0).Username);
+            Assert.AreEqual("User 2", actual.ElementAt(1).Username);
+            Assert.AreEqual("User 3", actual.ElementAt(2).Username);
+            Assert.AreEqual("User 4", actual.ElementAt(3).Username);
+        }
+
+        [Test]
+        public void Save_writes_the_user_to_the_database()
+        {
+            var user = new User
+                {
+                    Username = "new user", 
+                    LastLoginDate = new DateTime(2011, 5, 5, 5, 5, 5)
+                };
+
+            _repository.Save(user);
+
+            var actual = _repository.Get(user.Id);
+
+            Assert.AreEqual("new user", actual.Username);
+        }
+
+        [Test]
+        public void Save_updates_an_existing_user()
+        {
+            _user1.Username = "new username";
+
+            _repository.Save(_user1);
+
+            var actual = _repository.Get(_user1.Id);
+
+            Assert.AreEqual("new username", actual.Username);
+        }
+
+        [Test]
+        public void Save_updates_all_given_users()
+        {
+            _user1.Username = "new username";
+            _user2.Username = "new username 2";
+
+            var entities = new [] { _user1, _user2 };
+
+            _repository.Save(entities);
+
+            var actual1 = _repository.Get(_user1.Id);
+            var actual2 = _repository.Get(_user2.Id);
+
+            Assert.AreEqual("new username", actual1.Username);
+            Assert.AreEqual("new username 2", actual2.Username);
+        }
+
+        [Test]
+        public void Delete_by_user_removes_user_from_the_database()
+        {
+            _repository.Delete(_user1);
+
+            var actual = _repository.Get(_user1.Id);
+            
+            Assert.IsNull(actual);
+        }
+        
+        [Test]
+        public void Delete_by_user_id_removes_user_from_the_database()
+        {
+            _repository.Delete(_user1.Id);
+
+            var actual = _repository.Get(_user1.Id);
+
+            Assert.IsNull(actual);
         }
 
         [Test]
